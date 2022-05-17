702599e60341cd4b1513954cf181c26329c9fe33
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-05-17 12:35:51 +0900

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<div class = " class = headMenu2"><a href="#"><img src="cafe\img\logo.png"alt="" width="220px" height="35px"></a></div>
+
+<div class = " class = headMenu3">
+        <a href="#AtTheBeginning" style= text-decoration:none;>始めに</a>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="#Next" style= text-decoration:none;>体験</a>
+        &nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="" style= text-decoration:none;>お問い合わせ</a>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="" style= text-decoration:none; class = "space">サインイン</a>
+</div>
+
+
