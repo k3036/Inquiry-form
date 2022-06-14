4c6fee942a8881f1a36c0142859c68a46f1ab60c
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-06-14 19:35:30 +0900

    Update Inquiry-form.js

--- Inquiry-form.js
+++ Inquiry-form.js
@@ -1,26 +1,4 @@
 
-const headerMove = $('header') ;
-$(window).on("load scroll" , function (){
-    if($(this).scrollTop() > 100 && headerMove.hasClass("isFixed")==false){
-        headerMove.css("display","block");
-        headerMove.css({"top": "-80px" });
-        headerMove.addClass("isFixed");
-        headerMove.css({"top":0} , 600) ;
-    }
-    else if ($(this).scrollTop() < 80 && headerMove.hasClass("isFixed") == true){
-        headerMove.removeClass("isFixed");
-        headerMove.css("display","none");
-    }
-})
-
-
-
-window.addEventListener('scroll', function(){
-    window.scrollY
-});
-
-
-
 jQuery(function() {
     var appear = false;
     var pageTop = $('#page_top');
@@ -51,28 +29,108 @@ jQuery(function() {
 
 
 
+
+
+
+
+
+
+
+
+// ボタン、モダル、モダルの閉じるボタン、オーバーレイを変数に格納、モーダルウィンドウの出力
+let followSpace = document.querySelector('.followSpace');
+
+//モーダルウィンドウの出力
+let followModal = document.querySelector('.followModal');
+
+//バツ消しと外枠クリック消し
+let followCloseBtn = document.querySelector('.followCloseBtn');
+
+//モーダルウィンドウの外枠
+let followOverlay = document.querySelector('.followOverlay');
+
+// ボタンをクリックしたら、モダルとオーバーレイに.activeを付ける,モーダルウィンドウの表示。
+followSpace.addEventListener('click', function(e){
+  // aタグのデフォルトの機能を停止する
+  e.preventDefault();
+  // モーダルとオーバーレイにactiveクラスを付与する
+  followModal.classList.add('active');
+  followOverlay.classList.add('active');
+});
+
+// 画面以外をクリックすると消える。バツけしも消える。
+followOverlay.addEventListener('click', function() {
+followModal.classList.remove('active');
+followOverlay.classList.remove('active');
+});
+
+// ボタン、モーダルウィンドウ、モダルの閉じるボタン、オーバーレイを変数に格納、モーダルウィンドウの出力
+let space = document.querySelector('.space');
+
+//モーダルウィンドウの出力
+let modal = document.querySelector('.modal');
+
+//バツ消しと外枠クリック消し
+let closeBtn = document.querySelector('.close');
+
+//モーダルウィンドウの外枠
+let overlay = document.querySelector('.overlay');
+
+// ボタンをクリックしたら、モダルとオーバーレイに.activeを付ける,モーダルウィンドウの表示。
+space.addEventListener('click', function(e){
+  // aタグのデフォルトの機能を停止する
+  e.preventDefault();
+  // モーダルとオーバーレイにactiveクラスを付与する
+  modal.classList.add('active');
+  overlay.classList.add('active');
+});
+
+// 画面以外をクリックすると消える。バツけしも消える。
+overlay.addEventListener('click', function() {
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+});
 /**画面サイズにに関わらず画像サイズを固定 */
-  document.addEventListener('DOMContentLoaded', function () {
-    const resizeFix = function () {
-        const checkHeight = window.innerWidth / 1920 * 1080;
-        const marginTop = window.innerHeight - checkHeight;
-        const checkWidth = window.innerHeight / 1080 * 1920;
-        const marginLeft = window.innerWidth - checkWidth;
+document.addEventListener('DOMContentLoaded', function () {
+  const resizeFix = function () {
+      const checkHeight = window.innerWidth / 1920 * 1080;
+      const marginTop = window.innerHeight - checkHeight;
+      const checkWidth = window.innerHeight / 1080 * 1920;
+      const marginLeft = window.innerWidth - checkWidth;
 
-        if (checkHeight > window.innerHeight) { //
-            document.getElementsByClassName('sizeFixed')[0].style.transform = 'scale(' + window.innerHeight / 1080 + ')';
-            document.getElementsByClassName('sizeFixed')[0].style.marginLeft = marginLeft / 2 + 'px';
-            document.getElementsByClassName('sizeFixed')[0].style.marginTop = 0 + 'px';
-        } else {
-            document.getElementsByClassName('sizeFixed')[0].style.transform = 'scale(' + window.innerWidth / 1920 + ')';
-            document.getElementsByClassName('sizeFixed')[0].style.marginLeft = 0 + 'px';
-            document.getElementsByClassName('sizeFixed')[0].style.marginTop = marginTop / 2 + 'px';
-        }
+      if (checkHeight > window.innerHeight) { //
+          document.getElementsByClassName('sizeFixed')[0].style.transform = 'scale(' + window.innerHeight / 1080 + ')';
+          document.getElementsByClassName('sizeFixed')[0].style.marginLeft = marginLeft / 2 + 'px';
+          document.getElementsByClassName('sizeFixed')[0].style.marginTop = 0 + 'px';
+      } else {
+          document.getElementsByClassName('sizeFixed')[0].style.transform = 'scale(' + window.innerWidth / 1920 + ')';
+          document.getElementsByClassName('sizeFixed')[0].style.marginLeft = 0 + 'px';
+          document.getElementsByClassName('sizeFixed')[0].style.marginTop = marginTop / 2 + 'px';
+      }
+  }
+
+  resizeFix();
+
+  window.addEventListener('resize', function () {
+      resizeFix();
+  });
+});
+//追従するヘッダーの記述。
+const headerMove = $('header') ;
+$(window).on("load scroll" , function (){
+    if($(this).scrollTop() > 100 && headerMove.hasClass("isFixed")==false){
+        headerMove.css("display","block");
+        headerMove.css({"top": "-80px" });
+        headerMove.addClass("isFixed");
+        headerMove.css({"top":0} , 600) ;
+        headerMove.css({"z-index": "555" });
     }
+    else if ($(this).scrollTop() < 80 && headerMove.hasClass("isFixed") == true){
+        headerMove.removeClass("isFixed");
+        headerMove.css("display","none");
+    }
+})
 
-    resizeFix();
-
-    window.addEventListener('resize', function () {
-        resizeFix();
-    });
+window.addEventListener('scroll', function(){
+    window.scrollY;
 });
