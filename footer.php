81277790e4ac0305df1081c2f82990104307aa7d
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-07-06 15:09:47 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,136 +1,58 @@
-<footer>
-  <div class="MenuBelow">
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="form.css">
+  <style type="text/css">
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  </style>
+</head>
 
-    <p class=CorporateInformation>
-      企業情報
-    </p>
-
-    <p class=Community>
-      コミュニティ
-    </p>
-
-    <p class=Host>
-      ホスト
-    </p>
-
-    <p class=Support>
-      サポート
-    </p>
-
+<body>
+  <diV class="corporateInformation">
+    <p class="boldWord">企業情報</p>
+    <p>ご利用方法</p>
+    <p>ニュースルーム</p>
+    <p>株主・投資家のみなさまへ</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>採用情報</p>
+  </diV>
+  <diV class="corporateCommunity">
+    <p class="boldWord">コミュニティ</p>
+    <p>ダイバーシティ</p>
+    <p>アクセシビリティ対応</p>
+    <p>お友達を招待</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+  </diV>
+  <diV class="corporateHost">
+    <p class="boldWord">ホスト</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+  </diV>
+  <diV class="corporateSupport">
+    <p class="boldWord">サポート</p>
+    <p>新型コロナウイルスに対する取り組み</p>
+    <p>ヘルプセンター</p>
+    <p>キャンセルオプション</p>
+    <p>コミュニティサポート</p>
+    <p>信頼＆安全</p>
+  </diV>
+  <div class="copyrightFreedom">
+    <p>このサイトの素材は全て著作権フリーのものを使用しています。</p>
+    <p>プライバシーポリシー　利用規約　サイトマップ　企業情報</p>
+    <p>2021-LiNew,Inc. All rights reserved.</p>
   </div>
+</body>
 
-  <div class="FirstLineBelow">
-
-
-    <p class=Corporate>
-      ご利用方法
-    </p>
-
-    <p class=Diversity>
-      ダイバーシティ
-    </p>
-
-    <p class=XCharacter>
-      XXXXXXXXXXXXXXX
-    </p>
-
-    <p class=InitiativesForTheNewCoronavirus>
-      新型コロナウイルスに対する取り組み
-    </p>
-
-  </div>
-
-  <div class="SecondLine">
-
-
-    <p class=NewsRoom>
-      ニュースルーム
-    </p>
-
-    <p class=AccessibilitySupport>
-      アクセシビリティ対応
-    </p>
-
-    <p class=XCharacter2>
-      XXXXXXXXXXXXXXX
-    </p>
-
-    <p class=HelpCenter>
-      ヘルプセンター
-    </p>
-
-  </div>
-
-  <div class="ThirdLine">
-
-
-    <p class=ToShareholdersAndInvestors>
-      株主・投資家のみなさまへ
-    </p>
-
-    <p class=InviteYourFriends>
-      お友達を招待
-    </p>
-
-    <p class=XCharacter3>
-      XXXXXXXXXXXXXXX
-    </p>
-
-    <p class=CancellationOption>
-      キャンセルオプション
-    </p>
-
-  </div>
-
-
-  <div class="FourLine">
-
-    <p class=XCharacter4>
-      XXXXXXXXXXXXXXX
-    </p>
-    <p class=XCharacter5>
-      XXXXXXXXXXXXXXX
-    </p>
-    <p class=CommunitySupport>
-      コミュニティサポート
-    </p>
-
-  </div>
-
-  <div class="FiveLine">
-
-    <p class=XCharacter6>
-      XXXXXXXXXXXXXXX
-    </p>
-    <p class=XCharacter7>
-      XXXXXXXXXXXXXXX
-    </p>
-    <p class=TrustAndSafety>
-      信頼＆安全
-    </p>
-
-  </div>
-  <div class="SixLine">
-
-    <p class=XCharacter8>
-      XXXXXXXXXXXXXXX
-    </p>
-
-  </div>
-  <div class="EmploymentInformation">
-
-    <p class=Employment>
-      採用情報
-    </p>
-
-  </div>
-  <div class="footerWord">
-    <small class=footerWord2>このサイトの素材は全て著作権フリーのものを使用しています。</small>
-    <br>
-    <small class=footerWord1>プライバシーポリシー 利用規約 サイトマップ 企業情報</small>
-    <br>
-    <small class=footerWord0>&copy;2021- LiNew, Inc. All rights reserved.</small>
-    <br>
-  </div>
-</footer>
+</html>
