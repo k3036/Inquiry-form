6692fb7157f1665cf9fcf167b91b183bdeefe078
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-07-06 15:10:25 +0900

    Create footerTwo.php

---
+++ footerTwo.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="form.css">
+  <style type="text/css">
+  body {
+    margin: 0;
+    padding: 0;
+    text-align: center;
+  }
+  </style>
+</head>
+
+<body>
+  <diV class="corporateInformationTwo">
+    <p class="boldWord">企業情報</p>
+    <p>ご利用方法</p>
+    <p>ニュースルーム</p>
+    <p>株主・投資家のみなさまへ</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>採用情報</p>
+  </diV>
+  <diV class="corporateCommunityTwo">
+    <p class="boldWord">コミュニティ</p>
+    <p>ダイバーシティ</p>
+    <p>アクセシビリティ対応</p>
+    <p>お友達を招待</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+  </diV>
+  <diV class="corporateHostTwo">
+    <p class="boldWord">ホスト</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+    <p>xxxxxxxxxxxxxxxxxx</p>
+  </diV>
+  <diV class="corporateSupportTwo">
+    <p class="boldWord">サポート</p>
+    <p>新型コロナウイルスに対する取り組み</p>
+    <p>ヘルプセンター</p>
+    <p>キャンセルオプション</p>
+    <p>コミュニティサポート</p>
+    <p>信頼＆安全</p>
+  </diV>
+  <div class="copyrightFreedomTwo">
+    <p>このサイトの素材は全て著作権フリーのものを使用しています。</p>
+    <p>プライバシーポリシー　利用規約　サイトマップ　企業情報</p>
+    <p>2021-LiNew,Inc. All rights reserved.</p>
+  </div>
+</body>
+
+</html>
