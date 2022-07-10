0e8949357dfcf40dc68697e64ded2456ec4b94c6
K.kosuke <94692670+k3036@users.noreply.github.com>
2022-07-10 23:11:30 +0900

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -20,8 +20,6 @@
   <?php
     include "inquiryHeader3.php";
     ?>
-
-
   <main>
     <div class="contentOfTransmission2">
       <p class="inquiry">お問い合わせ</p>
@@ -32,11 +30,6 @@
       </div>
       <form method="POST" action="complete.php">
         <?php
-          /*session_start();
-          $session_id = session_id();
-          echo $session_id;*/
-
-
           $referer = $_SERVER["HTTP_REFERER"];   /*ダイレクトアクセスの禁止*/
           $url = 'inquiryScreen.php';
             if(!strstr($referer,$url)){
@@ -45,53 +38,11 @@
             }
         ?>
 
-
         <?php
-          /**スーパーグローバル変数を使うときの最初の記述 */
-          /**ファイルの先頭部に書かないとエラーが出る。 */
           function XssOCuntermeasure($XssMeasures)
           {
               echo nl2br(htmlspecialchars($XssMeasures, ENT_QUOTES, "UTF-8")) ;
           }
-        ?>
-
-        <?php
-        /* echo $_SESSION["chineseCharacterName"] ; */
-              /* データの受け取り */
-              // $_SESSION["chineseCharacterName"]	= $_POST["chineseCharacterName"];		//お名前
-              // $_SESSION["howToRead"]	= $_POST["howToRead"];        //読み仮名
-              // $_SESSION["phoneNumber"]		= $_POST["phoneNumber"];  //電話番号
-              // $_SESSION["emailAddress"]		= $_POST["emailAddress"];  //メールアドレス
-              // $_SESSION["contentsOfInquiry"]		= $_POST["contentsOfInquiry"];//お問い合わせ内容
-              $ten = 10 ;
-              if($_SESSION["chineseCharacterName"] === ""){
-                header("Location: inquiryScreen.php") ;
-                // echo "<FONT COLOR=\"RED\">※氏名は必須入力です。１０文字以内でご入力ください。</FONT>" ."</br>";
-              }else if(mb_strlen( $_SESSION["chineseCharacterName"]) >= $ten){
-                header("Location: inquiryScreen.php") ;
-                // echo "<FONT COLOR=\"RED\">※氏名は必須入力です。１０文字以内でご入力ください。</FONT>" ."</br>";
-              }
-              if($_SESSION["howToRead"] === ""){
-                header("Location: inquiryScreen.php") ;
-                // echo"<FONT COLOR=\"RED\">※フリガナは必須入力です。１０文字以内でご入力ください。</FONT>"."</br>" ;
-              }else if( mb_strlen($_SESSION["howToRead"]) >= $ten){
-                header("Location: inquiryScreen.php") ;
-                // echo"<FONT COLOR=\"RED\">※フリガナは必須入力です。１０文字以内でご入力ください。</FONT>"."</br>" ;
-              }
-              if(empty($_SESSION["phoneNumber"])){
-
-              }else if( !preg_match('/^0$|^-?[0-9][0-9]*$/', $_SESSION["phoneNumber"]	)){
-                header("Location: inquiryScreen.php") ;
-                // echo"<FONT COLOR=\"RED\">※電話番号は半角数字0-9のみでご入力ください。</FONT>" ."</br>";
-              }
-              if($_SESSION["emailAddress"]	 === ""){
-                header("Location: inquiryScreen.php") ;
-                // echo"<FONT COLOR=\"RED\">※メールアドレスは正しくご入力ください。</FONT>" ."</br>";
-              }
-              if( $_SESSION["contentsOfInquiry"] === ""){
-                header("Location: inquiryScreen.php") ;
-                // echo"<FONT COLOR=\"RED\">※お問い合わせ内容は必須入力です</FONT>" ;
-              }
         ?>
 
         <p class="fillInYourName">氏名</p>
@@ -117,7 +68,6 @@
         <p class="fillInYourEmailAddress">メールアドレス</p>
         <div class="Inquiry3LinesBelow"></div>
         <div class="Confirmation3"><?php XssOCuntermeasure($_SESSION["emailAddress"]	); ?></div>
-
         <p class="ContentsOfInquiry">お問い合わせ内容</p>
         <div class="Inquiry4LinesBelow"></div>
         <div class="Confirmation4"><?php XssOCuntermeasure($_SESSION["contentsOfInquiry"] ); ?></div>
@@ -132,11 +82,8 @@
           <p class="sendSize2"> 戻&nbsp;&nbsp;る</p>
         </a>
       </div>
-
     </div>
   </main>
-
-
   <div class="FooterList"></div>
   </main>
   <div class="FooterLine"></div>
